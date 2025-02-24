7a4252a9c50f8beec035267f2538901a9b99801b
jushinpon <jushinpon@gmail.com>
2025-02-24 18:28:30 +0800

    modify dlp test script to v3

--- scripts/DLP_test_suite4compressed.pl
+++ scripts/DLP_test_suite4compressed.pl
@@ -63,18 +63,31 @@ my @string = qq(
 #SBATCH --job-name=DLP_test4compress
 #SBATCH --nodes=1
 #SBATCH --partition=All
-source activate deepmd-cpu
+
+hostname
+
+if [ -f /opt/anaconda3/bin/activate ]; then
+    
+    source /opt/anaconda3/bin/activate deepmd-cpu-v3
+    export LD_LIBRARY_PATH=/opt/deepmd-cpu-v3/lib:/opt/deepmd-cpu-v3/lib/deepmd_lmp:\$LD_LIBRARY_PATH
+    export PATH=/opt/deepmd-cpu-v3/bin:\$PATH
+
+elif [ -f /opt/miniconda3/bin/activate ]; then
+    source /opt/miniconda3/bin/activate deepmd-cpu-v3
+    export LD_LIBRARY_PATH=/opt/deepmd-cpu-v3/lib:/opt/deepmd-cpu-v3/lib/deepmd_lmp:\$LD_LIBRARY_PATH
+    export PATH=/opt/deepmd-cpu-v3/bin:\$PATH
+else
+    echo "Error: Neither /opt/anaconda3/bin/activate nor /opt/miniconda3/bin/activate found."
+    exit 1  # Exit the script if neither exists
+fi
+
 node=1
-threads=2
+threads=\$(nproc)
 processors=\$(nproc)
 np=\$((\$node*\$processors/\$threads))
-export OMP_NUM_THREADS=\$np
-export TF_INTRA_OP_PARALLELISM_THREADS=\$np
-export TF_INTER_OP_PARALLELISM_THREADS=\$threads
-#The following are used only for intel MKL (not workable for AMD)
-export KMP_AFFINITY=granularity=fine,compact,1,0
-export KMP_BLOCKTIME=0
-export KMP_SETTINGS=TRUE
+
+export OMP_NUM_THREADS=\$processors
+export TF_INTRA_OP_PARALLELISM_THREADS=\$processors
 );
 
 map { s/^\s+|\s+$//g; } @string;
@@ -106,6 +119,7 @@ my ($lmp_hr) = @_;
 
 my $lmp_script = <<"END_MESSAGE";
 print "Simulating $lmp_hr->{input_data}"
+plugin load libdeepmd_lmp.so
 units metal 
 dimension 3 
 boundary p p p 
